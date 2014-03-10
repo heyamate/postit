1a4f3d965ecbf56cd446372f9b3e05156bc4a9a9
Heyamate <heyamate@gmail.com>
2014-03-10 18:23:05 +0800

    finish week 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140306095041) do
+ActiveRecord::Schema.define(version: 20140308031930) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140306095041) do
     t.text    "body"
     t.integer "post_id"
     t.integer "user_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
@@ -33,7 +40,9 @@ ActiveRecord::Schema.define(version: 20140306095041) do
   end
 
   create_table "users", force: true do |t|
-    t.string "username"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
